eae3ac3cf346bfe591d43f6956b3f3d612a269ce
sinofarmonov323 <farmonovsino4@gmail.com>
2025-08-29 15:39:13 +0500

    updated

--- main/models.py
+++ main/models.py
@@ -4,17 +4,28 @@ import os
 # Create your models here.
 
 class PostImage(models.Model):
-    title = models.CharField(max_length=200)
-    image = models.FileField(upload_to='images/')
+    title = models.CharField(max_length=200, blank=False, null=False)
+    file = models.FileField(upload_to='uploads/', blank=False, null=False)
 
     def __str__(self):
         return self.title
 
     def delete(self, *args, **kwargs):
-        print(self.image.path)
-        os.remove(self.image.path)
+        print(f"Object: {self.file} - args: {args}, kwargs: {kwargs}")
+        if self.file and os.path.isfile(self.file.path):
+            os.remove(self.file.path)
         super().delete(*args, **kwargs)
 
     def change(self, *args, **kwargs):
-        os.remove(self.image.path)
+        os.remove(self.file.path)
         super().change(*args, **kwargs)
+
+    def is_image(self):
+        return self.file.name.lower().endswith((".jpg", ".jpeg", ".png", ".webp"))
+        
+    def is_video(self):
+        return self.file.name.lower().endswith((".mp4", ".avi", ".mov", ".mkv"))
+
+    def save(self, force_insert = False, force_update = False, using = None, update_fields = None):
+        if self.is_image() or self.is_video():
+            super().save(force_insert, force_update, using, update_fields)
